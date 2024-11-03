09544bd3b5f5dede23bd722fe64f1367001960be
Code Warriors <159150600+codewarriorstm@users.noreply.github.com>
2024-11-03 12:05:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,10 +5,10 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "DId1QayS#szwnz9YoB7DyuRZ6_xC5UcK3kIvAEmQr6zefezlDnTc", //put your session id
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=DvQSnAAA#HCqnDhtLVJ7-giKZ4bDmk1o9Cit3RXlPZbfeiEeQ9l4", //put your session id
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/2nmi9q.png",
 ALIVE_MSG: process.env.ALIVE_MSG || "*👋 Hello I am nadeen poorna*",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",  //true or false
-MODE: process.env.MODE || "public", //private or public
+MODE: process.env.MODE || "private", //private or public
 AUTO_VOICE:"true" //true or false
 };
